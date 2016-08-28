7607782a8557cf7e1f5d67860d397e77c12485d1
Rabin Julien <julienr114@gmail.com>
2016-08-28 17:31:05 +0200

    install vue $ vuei18n package

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data () {
+      return {
+        lang: 'fr'
+      }
+    },
+    methods: {
+      setLang (lang) {
+        this.lang = lang
+      }
+    }
+  }
+</script>
+
+<template lang="jade">
+  h1 {{ $t("message.hello", lang) }}
+</template>
+
+<style>
+</style>
